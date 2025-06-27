ece86face86ab32ab9399d24aaee13954aea684a
Shanty <163443786+ShantyCerdasB@users.noreply.github.com>
2025-06-27 15:43:20 -0600

    deplying web app

--- apps/admin-web/vite.config.ts
+++ apps/admin-web/vite.config.ts
@@ -2,6 +2,7 @@
 import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
+import copy from 'rollup-plugin-copy'
 
 import path from 'path'
 import { fileURLToPath } from 'url'
@@ -13,11 +14,17 @@ export default defineConfig({
     react(),
     tailwind(),
     svgr(),
+    copy({
+      targets: [
+        { src: 'staticwebapp.config.json', dest: 'dist' }
+      ],
+      hook: 'writeBundle'
+    })
   ],
   resolve: {
     alias: {
-      '@':         path.resolve(__dirname, 'src'),
-      '@assets':   path.resolve(__dirname, 'src/assets'),
+      '@':           path.resolve(__dirname, 'src'),
+      '@assets':     path.resolve(__dirname, 'src/assets'),
       '@components': path.resolve(__dirname, 'src/components'),
     },
   },
